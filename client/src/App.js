a37974ae2e166e1d7ccc9b3c10f42654bed8fe5c
Megan D <45672063+megan-d@users.noreply.github.com>
2020-07-10 20:44:56 -0600

    Add additional dependencies, set up react-bootstrap, begin component structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import './styles/App.css';
+import React, { Fragment } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Footer from './components/layout/Footer';
+import './index.css';
 
 const App = () => {
-
   return (
-    <h1>Welcome to the Software Tracker! Glad to have you here!</h1>
+    <div className='full-page-grow'>
+      <h1>Welcome to the Software Tracker! Glad to have you here!</h1>
+      <Button>Try Me</Button>
+    </div>
+      
   );
-}
+};
 
 export default App;
---
+++ client/src/components/views/Error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Error = (props) => {
+  
+  return (
+    <div className='full-page'>
+      <h1 className='text-center text-results'>
+        Oops, something went wrong...
+      </h1>
+    </div>
+  );
+};
+
+export default Error;
