29d0832cd8a095eb45055a5e9f35b8a38f7f89d0
Megan D <45672063+megan-d@users.noreply.github.com>
2020-07-19 13:01:22 -0600

    Begin to complete register route and form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,17 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
+import axios from 'axios';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import PlainHeader from '../layout/PlainHeader';
-
-
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -31,88 +26,132 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: '300px',
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
 }));
 
-export default function SignUp() {
+export default function Register() {
   const classes = useStyles();
+  const [formData, updateFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    role: 'Developer',
+  });
+
+  //Pull out variables from formData
+  const { name, email, password, confirmPassword, role } = formData;
+
+  //Function to update state on change using updateFormData
+  const onChange = (e) =>
+    updateFormData({ ...formData, [e.target.name]: e.target.value });
+
+  //Function to send data that's in formData to database endpoint when submit is clicked
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const user = {
+      name: name,
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+      role: role,
+    };
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const body = JSON.stringify(user);
+    try {
+      //Axios will return promise with response in route to add new user (should return a token)
+      const res = await axios.post('/api/auth/register', body, config);
+    } catch (err) {
+      //If errors, get array of errors and loop through them and dispatch setAlert
+      const errors = err.response.data.errors;
+      if (errors) {
+        console.log(errors);
+      }
+    }
+  };
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component='main' maxWidth='xs'>
       <CssBaseline />
       <PlainHeader />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
-        <Typography component="h1" variant="h5">
+        <Typography component='h1' variant='h5'>
           Register
         </Typography>
-        <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-            
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up!
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
-            </Grid>
-          </Grid>
+        <form className='form-right' action='' onSubmit={(e) => onSubmit(e)}>
+          <div className='form-container-signup'>
+            <div className='form-group'>
+              <label>
+                Name:
+                <input
+                  type='text'
+                  name='name'
+                  placeholder=''
+                  value={name}
+                  onChange={(e) => onChange(e)}
+                  required
+                />
+              </label>
+            </div>
+            <div className='form-group'>
+              <label>
+                Email:
+                <input
+                  type='email'
+                  name='email'
+                  placeholder=''
+                  value={email}
+                  onChange={(e) => onChange(e)}
+                  required
+                />
+              </label>
+            </div>
+            <div className='form-group'>
+              <label>
+                Password:
+                <input
+                  type='password'
+                  name='password'
+                  placeholder=''
+                  value={password}
+                  onChange={(e) => onChange(e)}
+                  required
+                />
+              </label>
+            </div>
+            <div className='form-group'>
+              <label>
+                Confirm Password:
+                <input
+                  type='password'
+                  name='confirmPassword'
+                  placeholder=''
+                  value={confirmPassword}
+                  onChange={(e) => onChange(e)}
+                  required
+                />
+              </label>
+            </div>
+          </div>
+          <input
+            type='submit'
+            value='Sign Up'
+            className='button login-form-button'
+          />
         </form>
       </div>
     </Container>
---
+++ client/src/components/auth/RoleDropdown.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: '300px',
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+export default function SimpleSelect() {
+  const classes = useStyles();
+  const [role, setRole] = React.useState('');
+
+  const handleChange = (event) => {
+    setRole(event.target.value);
+  };
+
+  return (
+    <div>
+      
+      <FormControl className={classes.formControl}>
+        <InputLabel id="role-label">Role</InputLabel>
+        <Select
+          labelId="role"
+          id="role"
+          value={role}
+          onChange={handleChange}
+        >
+          <MenuItem value={'Developer'}>Developer</MenuItem>
+          <MenuItem value={'Manager'}>Manager</MenuItem>
+        </Select>
+        <FormHelperText>Please select your typical role on projects. This can be updated later.</FormHelperText>
+      </FormControl>
+      
+    </div>
+  );
+}
