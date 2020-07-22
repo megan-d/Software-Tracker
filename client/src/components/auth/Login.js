ad901563c7b00972a24a2e91aa32282ec3211e8b
Megan D <45672063+megan-d@users.noreply.github.com>
2020-07-21 22:58:23 -0600

    Style login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,13 +2,19 @@ import React, { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import axios from 'axios';
+import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import PlainHeader from '../layout/PlainHeader';
 import AlertBanner from '../layout/AlertBanner';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -27,13 +33,6 @@ const useStyles = makeStyles((theme) => ({
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
-  },
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: '300px',
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
   },
 }));
 
@@ -113,8 +112,63 @@ const Login = (props) => {
           Login
         </Typography>
         {userErrors && <AlertBanner errors={userErrors} />}
-        <form className='form' action='' onSubmit={(e) => onSubmit(e)}>
-          <div className='form-container'>
+        <form
+          className={classes.form}
+          action=''
+          onSubmit={(e) => onSubmit(e)}
+        >
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => onChange(e)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => onChange(e)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            
+            <Grid item>
+              <Link to="/register" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+};
+
+export default Login;
+
+{
+  /* <div className='form-container'>
             <div className='form-group'>
               <label>
                 Email:
@@ -152,9 +206,5 @@ const Login = (props) => {
           <p>Don't have an account?</p>
           <Link to='/register'>Sign Up</Link>
         </div>
-      </div>
-    </Container>
-  );
-};
-
-export default Login;
+      </div> */
+}
