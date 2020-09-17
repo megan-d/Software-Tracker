85b296fc2d33a5e4d9b253fef85b0a740d4eee8f
Megan D <45672063+megan-d@users.noreply.github.com>
2020-09-16 21:09:44 -0600

    Update token expiration

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -55,7 +55,7 @@ router.post(
       jwt.sign(
         payload,
         process.env.TOKEN_SECRET,
-        { expiresIn: '1000h' },
+        { expiresIn: '1h' },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
--- api/routes/users.js
+++ api/routes/users.js
@@ -117,7 +117,7 @@ router.post(
       jwt.sign(
         payload,
         process.env.TOKEN_SECRET,
-        { expiresIn: '1000h' },
+        { expiresIn: '1h' },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
